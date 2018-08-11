d3c7bedf988ba1762166989c668e187ccea3e961
Dezbraver <dezbraver@gmail.com>
2018-08-11 11:05:35 -0300

    Exe01-q3.py, q4.py, q5.py Releases

---
+++ Prog-Redes/Exe-Nivelamento#01/q4.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+x, y, r = 1, 0, 0
+
+try:
+	n = int(sys.argv[1])
+	if n <= 0: raise ValueError
+except (ValueError, IndexError):
+	print("Argumento de linha de comando invalido ou inexistente.")
+	while True:
+		try:
+			n = int(input("Digite um numero inteiro positivo: "))
+			if n <= 0: raise ValueError
+			os.system("clear")
+			break
+		except ValueError:
+			os.system("clear")
+			print("Valor inserido invalido tente novamente.")
+
+for i in range(0, n):
+	r = x + y
+	x = y
+	y = r
+	print(r, end = " ") 
+print()
+input("Tecle Enter para finalizar o programa...")
+os.system("clear")
---
+++ Prog-Redes/Exe-Nivelamento#01/q5.py
@@ -0,0 +1,17 @@
+import os
+
+vogais = ["a", "e", "i", "o", "u"]
+frase = input("Digite uma frase: ")
+nv, nc = 0, 0
+os.system("clear")
+for caractere in frase:
+	if caractere in vogais:
+		nv += 1
+	elif caractere.isalpha() == False:
+		pass
+	else:
+		nc += 1
+
+print("Nº de Caracteres (Espaços em branco contabilizados): {:>}\nNº de Vogais: {:>}\nNº de Consoantes: {:>}".format(len(frase), nv, nc))
+input("Tecle Enter para finalizar o programa...")
+os.system("clear")
